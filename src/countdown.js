f100ebaf1d8cccc2986d55c356ba051328e92051
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2023-09-27 23:43:00 +0600

    Add class in countdown
    Add class in countdown for customized timer
    
    Co-Authored-By: Iqbal Hossain <107544434+iqbal-xs@users.noreply.github.com>

--- src/countdown.js
+++ src/countdown.js
@@ -2,38 +2,49 @@ var Countdown = (function() {
 	var targetDate, targetElement;
   
 	function getTimeRemaining(endTime) {
-	  var totalSeconds = Math.max(Math.floor((endTime - Date.now()) / 1000), 0);
-	  var days = Math.floor(totalSeconds / (60 * 60 * 24));
-	  var hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-	  var minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-	  var seconds = totalSeconds % 60;
-	  return {
-		days: days,
-		hours: hours,
-		minutes: minutes,
-		seconds: seconds
-	  };
+		var totalSeconds = Math.max(Math.floor((endTime - Date.now()) / 1000), 0),
+			days = Math.floor(totalSeconds / (60 * 60 * 24)),
+			hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60)),
+			minutes = Math.floor((totalSeconds % (60 * 60)) / 60),
+			seconds = totalSeconds % 60;
+		return {
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds
+		};
 	}
   
 	function updateCountdown() {
-	  var time = getTimeRemaining(targetDate);
-	  var countdownText = time.days + 'd ' + time.hours + 'h ' + time.minutes + 'm ' + time.seconds + 's';
-	  targetElement.innerHTML = countdownText;
+		var time = getTimeRemaining(targetDate),
+			countdownWrap = targetElement.querySelector('.countdown-wrap');
+
+		if (!countdownWrap) {
+			countdownWrap = document.createElement('div');
+			countdownWrap.className = 'countdown-wrap';
+			targetElement.appendChild(countdownWrap);
+		}
+	
+		countdownWrap.innerHTML = `
+			<div class="days">${time.days}d</div>
+			<div class="hours">${time.hours}h</div>
+			<div class="minutes">${time.minutes}m</div>
+			<div class="seconds">${time.seconds}s</div>
+		`;
 	}
-  
+
 	function init(targetDateString, targetElementId) {
-	  targetDate = new Date(targetDateString).getTime();
-	  targetElement = document.getElementById(targetElementId);
-	  if (!targetElement) {
+		targetDate = new Date(targetDateString).getTime();
+		targetElement = document.getElementById(targetElementId);
+		if (!targetElement) {
 		console.error("Target element not found.");
 		return;
-	  }
-	  updateCountdown();
-	  setInterval(updateCountdown, 1000);
+		}
+		updateCountdown();
+		setInterval(updateCountdown, 1000);
 	}
-  
+
 	return {
-	  init: init
+		init: init
 	};
-  })();
-  
+})();
